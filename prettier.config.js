4964acde805e56014b9c78dd9a5ce2feb1a24bfb
Fernando Ayats <ayatsfer@gmail.com>
2025-04-11 23:37:16 +0200

    Add prettier astro

--- prettier.config.js
+++ prettier.config.js
@@ -8,4 +8,13 @@ export default {
     singleQuote: false,
     trailingComma: "all",
     semi: true,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
 };
